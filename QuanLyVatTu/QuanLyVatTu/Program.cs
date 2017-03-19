76b021c594b012dd8f1b99d1f5605185ba717962
namhv4 <utnam.ltmt1@gmail.com>
2017-03-19 22:57:50 +0700

    Test Git

--- QuanLyVatTu/QuanLyVatTu/Program.cs
+++ QuanLyVatTu/QuanLyVatTu/Program.cs
@@ -17,6 +17,7 @@ namespace QuanLyVatTu
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //cặc
         }
     }
 }
